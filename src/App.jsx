ba1f9005a33a63e530a5c48570aad53b8d96b2c5
Faria-10 <fariajabbar601@gmail.com>
2025-03-17 09:18:19 +0500

    background fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import Search from "./Components/Search";
+import Search from "./components/search";
 import Test from "./Components/Test";
 import Weather from "./components/weather";
 import Map from "./Components/Map";
@@ -9,12 +9,16 @@ const App = () => {
   const [city, setCity] = useState("London, United States"); 
 
   return (
+    
     <>
-      <div className="mx-auto  max-w-screen-md mt-4 py-5 bg-gradient-to-br from bg-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <div className="mx-auto  max-w-screen-md mt-4 pt-5  h-fit bg-gradient-to-br from bg-cyan-700 to-blue-700 shadow-xl shadow-gray-400">
         <Search onSearch={setCity} />
         <Weather cityName={city} />
         <Test cityName={city} />  
-    <Map/>
+        <Map/>
+    
+    
+    
       </div>
     </>
   );
--- src/Components/TimeandLocation.jsx
+++ src/Components/TimeandLocation.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const TimeandLocation = ({ dateTime }) => {
   return (
     <div>
-      <div className="flex items-center justify-center my-6">
+      <div className="flex items-center justify-center my-6 ">
         <p className="text-white text-xl font-extralight">
           {/* YA CONDITON LAGAI HOI TRUE FALSE KI KY AGAR TIME AVAILABILE HY TO A JAY GA WRNA FETCHING A JAY GA? */}
           {dateTime ? dateTime : "Fetching time..."}
@@ -11,6 +11,6 @@ const TimeandLocation = ({ dateTime }) => {
       </div>
     </div>
   );
-};
+}; 
 
 export default TimeandLocation;
