f384831cf5941a5358bc3475edecb748f845a798
Faria-10 <fariajabbar601@gmail.com>
2025-03-18 09:34:33 +0500

    fix build issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import React, { useState } from "react";
-
-import Search from "./components/search";
-import Test from "./Components/Test";
+import React, { useState, lazy, Suspense } from "react";
+import Search from "./components/Search";
 import Weather from "./components/weather";
-import Map from "./Components/Map";
+
+// Lazy loading components
+const Test = lazy(() => import("./components/Test"));
+const Map = lazy(() => import("./Components/Map"));
 
 const App = () => {
   const [city, setCity] = useState("London, United States"); 
 
   return (
-    
-    <>
-      <div className="mx-auto  max-w-screen-md mt-4 pt-5  h-fit bg-gradient-to-br from bg-cyan-700 to-blue-700 shadow-xl shadow-gray-400">
-        <Search onSearch={setCity} />
-        <Weather cityName={city} />
+    <div className="mx-auto  max-w-screen-md mt-4 pt-5  h-fit bg-gradient-to-br from bg-cyan-700 to-blue-700 shadow-xl shadow-gray-400">
+      <Search onSearch={setCity} />
+      <Weather cityName={city} />
+      <Suspense fallback={<div>Loading Test...</div>}>
         <Test cityName={city} />  
-        <Map/>
-      </div>
-    </>
+      </Suspense>
+      <Suspense fallback={<div>Loading Map...</div>}>
+        <Map />
+      </Suspense>
+    </div>
   );
 };
 
